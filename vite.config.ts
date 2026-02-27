3ca2a8c398ddf22666ca1746323a18b592807c97
Antonio <AntonioJoseAliagaMolina@gmail.com>
2026-02-27 18:48:44 +0000

    Arreglo definitivo de rutas relativas

--- vite.config.ts
+++ vite.config.ts
@@ -6,18 +6,13 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/calculadoraderally/',
+    // Asegúrate de que el nombre del repo sea exacto (minúsculas)
+    base: '/calculadoraderally/', 
     plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(__dirname, './src'),
       },
-    },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
     },
   };
 });

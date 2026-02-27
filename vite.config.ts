e43bed361b81eecc8c520e1788790f5b8c527016
Antonio <AntonioJoseAliagaMolina@gmail.com>
2026-02-27 19:05:50 +0000

    Prueba de emergencia con rutas relativas

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    // Asegúrate de que el nombre del repo sea exacto (minúsculas)
-    base: '/calculadoraderally/', 
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
+export default defineConfig({
+  // El punto y barra hace que las rutas sean relativas y funcionen siempre
+  base: './', 
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-  };
-});
+  },
+})
